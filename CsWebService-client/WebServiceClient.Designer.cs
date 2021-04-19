da1822d3006c633e2d5a02ae367aaa4fb29449d6
Vinh Nguyen <vinhnguyen.goid2@gmail.com>
2021-04-19 10:57:13 +0700

    add UI and function for all button

---
+++ CsWebService-client/WebServiceClient.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace CsWebService_client
+{
+    partial class WebServiceClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumA = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtNumB = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNumA
+            // 
+            this.txtNumA.Location = new System.Drawing.Point(161, 38);
+            this.txtNumA.Name = "txtNumA";
+            this.txtNumA.Size = new System.Drawing.Size(204, 20);
+            this.txtNumA.TabIndex = 0;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(55, 41);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(17, 13);
+            this.lblA.TabIndex = 1;
+            this.lblA.Text = "A:";
+            // 
+            // txtNumB
+            // 
+            this.txtNumB.Location = new System.Drawing.Point(161, 80);
+            this.txtNumB.Name = "txtNumB";
+            this.txtNumB.Size = new System.Drawing.Size(204, 20);
+            this.txtNumB.TabIndex = 0;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(55, 83);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(17, 13);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "B:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 122);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(117, 122);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 23);
+            this.btnSub.TabIndex = 2;
+            this.btnSub.Text = "Subtract";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(224, 122);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(75, 23);
+            this.btnMul.TabIndex = 2;
+            this.btnMul.Text = "Multiply";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(337, 122);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 23);
+            this.btnDiv.TabIndex = 2;
+            this.btnDiv.Text = "Divide";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(55, 167);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(43, 13);
+            this.lblRes.TabIndex = 1;
+            this.lblRes.Text = "Result: ";
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(161, 164);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(204, 20);
+            this.txtRes.TabIndex = 0;
+            // 
+            // WebServiceClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 208);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtNumB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txtNumA);
+            this.Name = "WebServiceClient";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.WebServiceClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumA;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtNumB;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.TextBox txtRes;
+    }
+}
+
---
+++ CsWebService-client/WebServiceClient.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsWebService_client
+{
+    public partial class WebServiceClient : Form
+    {
+        public WebServiceClient()
+        {
+            InitializeComponent();
+        }
+
+        private void WebServiceClient_Load(object sender, EventArgs e)
+        {
+
+        }
+        //private int getData(double a, double b)
+        //{
+        //    double
+        //    return a;
+        //}
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            CsWebService_client.CalcRef.CalculatorWebServiceSoapClient calc = new CalcRef.CalculatorWebServiceSoapClient();
+            double a = double.Parse(txtNumA.Text);
+            double b = double.Parse(txtNumB.Text);
+            double result = calc.add(a, b);
+            txtRes.Text = "" + result;
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            CsWebService_client.CalcRef.CalculatorWebServiceSoapClient calc = new CalcRef.CalculatorWebServiceSoapClient();
+            double a = double.Parse(txtNumA.Text);
+            double b = double.Parse(txtNumB.Text);
+            double result = calc.subtract(a, b);
+            txtRes.Text = "" + result;
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            CsWebService_client.CalcRef.CalculatorWebServiceSoapClient calc = new CalcRef.CalculatorWebServiceSoapClient();
+            double a = double.Parse(txtNumA.Text);
+            double b = double.Parse(txtNumB.Text);
+            double result = calc.multiply(a, b);
+            txtRes.Text = "" + result;
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            CsWebService_client.CalcRef.CalculatorWebServiceSoapClient calc = new CalcRef.CalculatorWebServiceSoapClient();
+            double a = double.Parse(txtNumA.Text);
+            double b = double.Parse(txtNumB.Text);
+            if (b == 0) MessageBox.Show("Divide by zero");
+            else
+            {
+                double result = calc.divide(a, b);
+                txtRes.Text = "" + result;
+            }
+            
+        }
+    }
+}
